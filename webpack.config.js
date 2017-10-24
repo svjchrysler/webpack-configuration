499793b177f843e8204bc5756c99d0d2f9b5d456
Jhon Salguero <svjchrysler@gmail.com>
2017-10-24 16:00:35 -0400

    added new script to build application

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
     entry: ['./src/index.js', './src/style.scss'],
@@ -19,7 +20,8 @@ module.exports = {
                 loader: ExtractTextPlugin.extract([
                     'css-loader',
                     'sass-loader'
-                ])
+                ]),
+                exclude: /node_modules/
             }
         ]
     },
@@ -27,6 +29,10 @@ module.exports = {
         new ExtractTextPlugin({
             filename: '[name].bundle.css',
             allChunks: true
+        }),
+        new UglifyJSPlugin({
+            cache: true,
+            sourceMap: true
         })
     ],
     devServer: {
